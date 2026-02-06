54b50f3f49b12c5d7f4c222c20f0f8fe28b25703
D4XTERxAURA <kmausm385@gmail.com>
2026-02-07 00:12:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "30742749"  # Replace this API ID with your actual API ID
+API_HASH = "64aad5eb84c7d7d57c25866c9001544c"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "8330141445:AAHfeGJguFyCRgS6kBo7HQg1Yb-zaEtk_no"  # Replace this BOT_TOKEN
+
+# Admin IDs
+ADMIN_IDS = [7869729211, 12345678]
+
+# Limits
+DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
+ADMIN_LIMIT = 50000 # Card Scrapping Limit For Admin
